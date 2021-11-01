767d7033353589b29d41cd4f0ec0bed3fb4372d2
Mitchell <Hmitchell.hall@gmail.com>
2021-11-01 16:28:53 -0500

    commit tree e1a4dc053497471ef36d17c32da06d05441207af

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+
+  def initialize side_one, side_two, side_three
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    if ((@side_one + @side_two + @side_three)/@side_one.to_f == 3.0) && (a_triangle? == true)
+      :equilateral
+    elsif ((@side_one == @side_two) || (@side_three == @side_two) || (@side_three == @side_one)) && (a_triangle? == true)
+      :isosceles
+    elsif a_triangle?
+      :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  def a_triangle?
+    if (@side_one + @side_two > @side_three) && (@side_one + @side_three > @side_two) && (@side_two + @side_three > @side_one)
+      true
+    else
+      false
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "invalid traingle"
+  end
 end
